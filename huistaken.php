9229c7c496debbe5db1eee5a57592a6b39885999
Yorickbusser <43119846+Yorickbusser@users.noreply.github.com>
2019-01-18 20:07:54 +0100

    Update huistaken.php
    huistaken ingevuld voor bijna iedereen

--- huistaken.php
+++ huistaken.php
@@ -11,27 +11,27 @@
 
   <div class="profiellijstitem">
     <img src="afbeeldingen/img1.jpg" class="profielfoto">
-    <h1 style="line-height:200px;">Trap en hal</h1>
+    <h1 style="line-height:200px;">Trap en overloop</h1>
   </div>
   <div class="profiellijstitem">
-    <img src="afbeeldingen/img1.jpg" class="profielfoto">
-    <h1 style="line-height:200px;">Trap en hal</h1>
+    <img src="afbeeldingen/yana.jpg" class="profielfoto">
+    <h1 style="line-height:200px;">Toilet beneden</h1>
   </div>
   <div class="profiellijstitem">
-    <img src="afbeeldingen/img1.jpg" class="profielfoto">
-    <h1 style="line-height:200px;">Trap en hal</h1>
+    <img src="afbeeldingen/femke.jpg" class="profielfoto">
+    <h1 style="line-height:200px;">Koelkast rechts</h1>
   </div>
   <div class="profiellijstitem">
-    <img src="afbeeldingen/img1.jpg" class="profielfoto">
-    <h1 style="line-height:200px;">Trap en hal</h1>
+    <img src="afbeeldingen/EJbrak.jpg" class="profielfoto">
+    <h1 style="line-height:200px;">Toilet boven</h1>
   </div>
   <div class="profiellijstitem">
-    <img src="afbeeldingen/img1.jpg" class="profielfoto">
-    <h1 style="line-height:200px;">Trap en hal</h1>
+    <img src="afbeeldingen/joris.jpg" class="profielfoto">
+    <h1 style="line-height:200px;">Hal</h1>
   </div>
   <div class="profiellijstitem">
-    <img src="afbeeldingen/img1.jpg" class="profielfoto">
-    <h1 style="line-height:200px;">Trap en hal</h1>
+    <img src="afbeeldingen/emma.jpg" class="profielfoto">
+    <h1 style="line-height:200px;">Keukenvloer</h1>
   </div>
   <div class="profiellijstitem">
     <img src="afbeeldingen/img1.jpg" class="profielfoto">
